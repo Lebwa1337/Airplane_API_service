ed8a3b731fbf805eaff92bd43a9c504562293e9f
LeBwa <lebwa1337@gmail.com>
2024-03-13 13:27:16 +0200

    Create custom user model by changing email as username

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
