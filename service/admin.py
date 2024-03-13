7f91449a0777f96d7591e9a3f9757cdf39b6f825
LeBwa <lebwa1337@gmail.com>
2024-03-13 14:57:14 +0200

    add models to admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from service.models import *
+
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Crew)
+admin.site.register(Route)
+admin.site.register(Airport)
+admin.site.register(Ticket)
+admin.site.register(Order)
+admin.site.register(Flight)
+admin.site.register(Country)
+admin.site.register(City)
