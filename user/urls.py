c1c1c5023c998da8204cb5522df1b76979e9dde3
LeBwa <lebwa1337@gmail.com>
2024-03-23 15:56:24 +0200

    return register endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register_user"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("me/", ManageUserView.as_view(), name="manage"),
