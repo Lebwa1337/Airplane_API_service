c192705dc3a53f06c86f7fbb4f66446e2341131e
LeBwa <lebwa1337@gmail.com>
2024-03-25 19:52:32 +0200

    Get secret_key form environment

--- airplane_service/settings.py
+++ airplane_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x=^8_s-z!%+rg0eaui8^x^qblo**jmam6iblacn_1kbn5_r)94"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
